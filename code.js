ab050f461922a45899521fac132e36593bbc8fe1
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-03-04 12:52:49 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function depthFirstSearch(graph, startNode, targetNode) {
+function depthFirstSearch(graph, startNode, targetNode, visited = []) {
+    visited.push(startNode);
+
+    if (startNode === targetNode) {
+        return [startNode];
+    }
+
+    for (var i = 0; i < graph[startNode].length; i++) {
+        if (!visited.includes(graph[startNode][i])) {
+            var path = depthFirstSearch(graph, graph[startNode][i], targetNode, visited.slice());
+            if (path.length > 0) {
+                return [startNode].concat(path);
+            }
+        }
+    }
+
     return [];
 }
